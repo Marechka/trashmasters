bf2506aa7a5cb4423fb1a307f50ca37af009bf9c
Masha <39933741+Marechka@users.noreply.github.com>
2026-01-25 10:31:41 -0800

    initial commit

---
+++ src/main/java/com/app/trashmasters/TrashmastersApplication.java
@@ -0,0 +1,13 @@
+package com.app.trashmasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashmastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashmastersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/trashmasters/TrashmastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.trashmasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashmastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
